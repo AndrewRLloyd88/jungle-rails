0d5cc6a7b1c764e98f125ddc015fc22111267ed3
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-13 14:46:48 -0700

    added the product count and category count to the admin panel on jungle. Made some CSS tweaks to make the admin dashboard more presentable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,8 @@ class Admin::DashboardController < ApplicationController
   password: ENV["HTTP_BASIC_PASSWORD"],
   if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
   def show
+    #controller is fetching the data and making it available to the views automagically
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
