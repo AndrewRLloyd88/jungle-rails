5043e1349be6991575be2a8defde3ffc9b704935
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-13 12:26:24 -0700

    secure the admin portion of the site by adding on basic HTTP authorization

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ProductsController < ApplicationController
+ #adding basic http authentication for admins
+ http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+ password: ENV["HTTP_BASIC_PASSWORD"],
+ if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def index
     @products = Product.order(id: :desc).all
